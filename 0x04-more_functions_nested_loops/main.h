a493c93f2db00fafd7676480816d1cbea8d96bd1
Your Name <you@example.com>
2023-03-23 16:35:18 -0700

    loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
 #define MAIN_H
-
 /*
  * File: main.h
  * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x03-more_functions_nested_loops directory.
  */
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -19,5 +17,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
 #endif
