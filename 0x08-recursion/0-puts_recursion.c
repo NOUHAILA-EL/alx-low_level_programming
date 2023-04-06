57081b0b7109578669e7809789f35ecff5190fd5
Your Name <you@example.com>
2023-04-05 17:07:12 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-void _puts_recursion(char *c)
+void _puts_recursion(char *s)
 {
-	if (*c)
+	if (*s)
 	{
-		_putchar(*c);
-		_puts_recursion(c + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 	else
